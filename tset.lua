ae4852959bd720ed1f5eca2708960425cb726d95
Griffin // vykzs <89820610+vyks@users.noreply.github.com>
2021-09-11 21:09:47 -0400

    test

--- tset.lua
+++ tset.lua
@@ -1,1 +1,64 @@
+local Library = {}
 
+function Library:CreateMain()
+    local ScreenGui = Instance.new("ScreenGui")
+    local Frame = Instance.new("Frame")
+    local Frame_2 = Instance.new("Frame")
+    local Frame_3 = Instance.new("Frame")
+    local UIListLayout = Instance.new("UIListLayout")
+    local TextLabel = Instance.new("TextLabel")
+
+    ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+    Frame.Parent = ScreenGui
+    Frame.BackgroundColor3 = Color3.fromRGB(60, 62, 66)
+    Frame.BorderSizePixel = 0
+    Frame.Position = UDim2.new(0.260765553, 0, 0.301886797, 0)
+    Frame.Size = UDim2.new(0, 400, 0, 235)
+
+    Frame_2.Parent = Frame
+    Frame_2.BackgroundColor3 = Color3.fromRGB(48, 50, 54)
+    Frame_2.BorderSizePixel = 0
+    Frame_2.Position = UDim2.new(0.0125000002, 0, 0.129999995, 0)
+    Frame_2.Size = UDim2.new(0, 390, 0, 200)
+
+    Frame_3.Parent = Frame_2
+    Frame_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    Frame_3.BackgroundTransparency = 1.000
+    Frame_3.Position = UDim2.new(0.012820513, 0, 0.0250000004, 0)
+    Frame_3.Size = UDim2.new(0, 380, 0, 190)
+
+    UIListLayout.Parent = Frame_3
+    UIListLayout.FillDirection = Enum.FillDirection.Horizontal
+    UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+
+    TextLabel.Parent = Frame
+    TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.BackgroundTransparency = 1.000
+    TextLabel.Position = UDim2.new(0.25, 0, 0, 0)
+    TextLabel.Size = UDim2.new(0, 200, 0, 30)
+    TextLabel.Font = Enum.Font.SourceSans
+    TextLabel.Text = "Vykzs"
+    TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.TextSize = 20.000
+
+    local GamerLibrary = {}
+    
+    function GamerLibrary:NewButton(name)
+        local TextButton = Instance.new("TextButton")
+
+        TextButton.Parent = Frame_3
+        TextButton.BackgroundColor3 = Color3.fromRGB(76, 76, 76)
+        TextButton.BorderSizePixel = 0
+        TextButton.Size = UDim2.new(0, 190, 0, 50)
+        TextButton.Font = Enum.Font.SourceSans
+        TextButton.Text = name
+        TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+        TextButton.TextSize = 14.000
+    end
+
+    return GamerLibrary
+end
+
+return Library
